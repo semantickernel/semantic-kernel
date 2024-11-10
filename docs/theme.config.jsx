1ff21da092325c380f59bee156b5d990e9937250
Afshin Mehrabani <afshin.meh@gmail.com>
2024-11-10 10:52:00 +0000

    Docs bootstrap

---
+++ docs/theme.config.jsx
@@ -0,0 +1,7 @@
+export default {
+  logo: <span>Semantic Kernel for JavaScript</span>,
+  docsRepositoryBase: 'https://github.com/semantickernel/semantic-kernel/tree/main/docs',
+  project: {
+    link: "https://github.com/semantickernel/semantic-kernel",
+  },
+};
