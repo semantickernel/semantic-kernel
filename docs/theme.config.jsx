b6d6dff8386686d24d15ec616ced553bad77cbd2
Afshin Mehrabani <afshin.meh@gmail.com>
2024-11-10 15:01:37 +0000

    docs: header

--- docs/theme.config.jsx
+++ docs/theme.config.jsx
@@ -1,3 +1,6 @@
+import { useRouter } from 'next/router';
+import { useConfig } from 'nextra-theme-docs';
+
 export default {
   logo: (
     <>
@@ -8,6 +11,41 @@ export default {
   docsRepositoryBase: 'https://github.com/semantickernel/semantic-kernel/tree/main/docs',
   project: {
     link: 'https://github.com/semantickernel/semantic-kernel',
+  },
+  head: function useHead() {
+    const config = useConfig();
+    const { route } = useRouter();
+    const image = 'kernel-infographic.jpeg';
+
+    const description =
+      config.frontMatter.description ||
+      'Semantic Kernel is a lightweight, open-source development kit that lets you easily build AI agents and integrate the latest AI models';
+    const title = config.title + (route === '/' ? '' : ' - Semantic Kernel for JavaScript');
+
+    return (
+      <>
+        <title>{title}</title>
+        <meta property="og:title" content={title} />
+        <meta name="description" content={description} />
+        <meta property="og:description" content={description} />
+        <meta property="og:image" content={image} />
+
+        <meta name="msapplication-TileColor" content="#fff" />
+        <meta httpEquiv="Content-Language" content="en" />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:site:domain" content="kerneljs.com" />
+        <meta name="twitter:url" content="https://kerneljs.com" />
+        <meta name="apple-mobile-web-app-title" content="kerneljs" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <link rel="manifest" href="/site.webmanifest" />
+      </>
+    );
+  },
+  sidebar: {
+    defaultMenuCollapseLevel: 1,
+    toggleButton: true,
   },
   themeSwitch: {
     useOptions() {
