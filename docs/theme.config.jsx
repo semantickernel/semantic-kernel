d9f4b2aebec222996dd8005b28c34b5ec7b73cad
Afshin Mehrabani <afshin.meh@gmail.com>
2024-11-14 08:08:10 +0000

    docs: git move

--- docs/theme.config.jsx
+++ docs/theme.config.jsx
@@ -8,9 +8,9 @@ export default {
       <span style={{ marginLeft: '.4em', fontWeight: 800 }}>Semantic Kernel for JavaScript</span>
     </>
   ),
-  docsRepositoryBase: 'https://github.com/semantickernel/semantic-kernel/tree/main/docs',
+  docsRepositoryBase: 'https://github.com/afshinm/semantic-kernel/tree/main/docs',
   project: {
-    link: 'https://github.com/semantickernel/semantic-kernel',
+    link: 'https://github.com/afshinm/semantic-kernel',
   },
   head: function useHead() {
     const config = useConfig();
